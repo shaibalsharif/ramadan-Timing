cb3d0772ff183e5c6e7dfc776cbae91d42b07503
shaibal-tiller <shaibal.tiller@gmail.com>
2024-03-14 14:07:52 +0600

    first deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { ramadan_data } from './data';
+import IfterCard from './IfterCard';
+import SahriCard from './SahriCard';
+
+const iftertext ="ইফতার এর বাকি "
+const sahritext ="সেহরির  বাকি "
+
+const calculate_hrs_mins = (differenceInMilliseconds) => {
+  const hours = Math.floor((differenceInMilliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) - 12;
+  const minutes = Math.floor((differenceInMilliseconds % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((differenceInMilliseconds % (1000 * 60)) / 1000);
+  return { hours, minutes, seconds };
+};
+
+const getTimeRemaining = (current, sahri, ifter) => {
+  const sahri_time_diff = sahri - current;
+  const ifter_time_diff = ifter - current;
+  return {
+    sahri: calculate_hrs_mins(sahri_time_diff),
+    ifter: calculate_hrs_mins(ifter_time_diff),
+  };
+};
 
 const App = () => {
-  const [timeRemaining, setTimeRemaining] = useState('');
-  const current_data = ramadan_data.find(el => {
+  const [timeRemaining, setTimeRemaining] = useState(null);
+  const current_data = ramadan_data.find((el) => {
     const currentDateObj = new Date();
     const elDateObj = new Date(el.date);
     return (
@@ -13,39 +34,65 @@ const App = () => {
       currentDateObj.getFullYear() === elDateObj.getFullYear()
     );
   });
+
   useEffect(() => {
-    /* const interval = setInterval(() => { */
-    const currentDate = new Date();
-    const sahriEndsDate = new Date(currentDate.getFullYear(),
-      currentDate.getMonth() + 1,
-      currentDate.getDate(),
-      parseInt(current_data.sahri_ends.split('-')[0]),
-      parseInt(current_data.sahri_ends.split('-')[1]),
-      0
+    const interval = setInterval(() => {
+      const currentDate = new Date();
+      const sahriEndsDate = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() + 1,
+        currentDate.getDate(),
+        parseInt(current_data.sahri_ends.split('-')[0]),
+        parseInt(current_data.sahri_ends.split('-')[1]),
+        0
+      );
+      const iftaarstartsDate = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() + 1,
+        currentDate.getDate(),
+        parseInt(current_data.iftar_starts.split('-')[0]),
+        parseInt(current_data.iftar_starts.split('-')[1]),
+        0
+      );
 
 
-    );
+      if (currentDate.getHours() > 22 && currentDate.getHours() < 6) {
+        const time = getTimeRemaining(currentDate, sahriEndsDate, iftaarstartsDate);
+        setTimeRemaining({ type: 'sahri', time });
+      } else if (currentDate.getHours() > 6 && currentDate.getHours() < 19) {
+        const time = getTimeRemaining(currentDate, sahriEndsDate, iftaarstartsDate);
+        setTimeRemaining({ type: 'ifter', time });
+      } else {
+        const nextSahriDate = new Date(currentDate);
+        nextSahriDate.setDate(nextSahriDate.getDate() + 1);
+        const nextSahriEndsDate = new Date(
+          nextSahriDate.getFullYear(),
+          nextSahriDate.getMonth() + 1,
+          nextSahriDate.getDate(),
+          parseInt(current_data.sahri_ends.split('-')[0]),
+          parseInt(current_data.sahri_ends.split('-')[1]),
+          0);
+        const time = getTimeRemaining(currentDate, nextSahriEndsDate, iftaarstartsDate);
+        setTimeRemaining({ type: 'sahri', time });
+      }
+    }, 1000);
 
-    const difference = currentDate - sahriEndsDate;
-    console.log(currentDate,sahriEndsDate);
-    if (difference > 0) {
-      const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-      console.log(hours);
-      setTimeRemaining(`${hours}:${minutes}:${seconds}`);
-    } else {
-      setTimeRemaining('Sahri time ended');
-    }
-  }, []);
+    return () => clearInterval(interval);
+  }, [current_data]);
 
-
+  if (!timeRemaining) {
+    return null; // Or loading indicator
+  }
 
   return (
     <div className="h-screen w-full bg-[#52525256] overflow-hidden">
-      <div>Time remaining until Sahri ends: {timeRemaining}</div>
+      {timeRemaining.type === 'sahri' ? (
+        <SahriCard text={timeRemaining.type=='sahri'?sahritext:""} time={timeRemaining.time.sahri} />
+      ) : (
+        <IfterCard text={timeRemaining.type=='ifter'?iftertext:""} time={timeRemaining.time.ifter} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/IfterCard.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const IfterCard = ({ text, time }) => {
+    return (
+        <div className=''>
+  <h2 className="title-header text-center pt-56 ">{text}</h2>
+            <div className="digitalClock  ">
+                <div className="timer">
+                    <div className="time">TIME
+
+                    </div>
+                    <div id="hours" className="t">
+                        {time?.hours}<br />
+                        hours
+                    </div>
+                    <div className="colon">:</div>
+                    <div id="mins" className="t">
+                        {time?.minutes} <br />
+                        mins
+                    </div>
+                    <div className="colon">:</div>
+                    <div id="secs" className="t">
+                        {time?.seconds}<br />
+                        secs
+                    </div>
+
+                </div>
+                <div className="boxOne">
+                  
+                </div>
+                <div className="boxTwo">
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default IfterCard
